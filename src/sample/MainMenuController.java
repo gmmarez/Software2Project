e499b5a87645e9f38ff8e52d174a0aca21761793
gmmarez <gabrielmarez123@gmail.com>
2023-02-09 16:50:00 -0700

    2/9/23 6:49pm

--- src/sample/MainMenuController.java
+++ src/sample/MainMenuController.java
@@ -2,13 +2,18 @@ package sample;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class MainMenuController implements initializable {
+public class MainMenuController implements Initializable {
     @FXML
     private Button MainMenuCustomers;
 
@@ -25,7 +30,7 @@ public class MainMenuController implements initializable {
     Parent scene;
 
     @FXML
-    void MainMenuCustomers(ActionEvent actionEvent) throws IOException {
+    void MainMenuCustomers(ActionEvent event) throws IOException {
         // Goes to Customers screen
 
         stage = (Stage) ((Button)event.getSource()).getScene().getWindow();
@@ -35,7 +40,7 @@ public class MainMenuController implements initializable {
     }
 
     @FXML
-    void MainMenuAppointments(ActionEvent actionEvent) throws IOException {
+    void MainMenuAppointments(ActionEvent event) throws IOException {
         // Goes to Appointments screen
 
         stage = (Stage) ((Button)event.getSource()).getScene().getWindow();
@@ -45,7 +50,7 @@ public class MainMenuController implements initializable {
     }
 
     @FXML
-    void MainMenuRecords(ActionEvent actionEvent) throws IOException {
+    void MainMenuRecords(ActionEvent event) throws IOException {
         // Goes to Records screen
 
         stage = (Stage) ((Button)event.getSource()).getScene().getWindow();
@@ -55,12 +60,17 @@ public class MainMenuController implements initializable {
     }
 
     @FXML
-    void MainMenuLogout(ActionEvent actionEvent) throws IOException {
+    void MainMenuLogout(ActionEvent event) throws IOException {
         // Goes back to Login screen
 
         stage = (Stage) ((Button)event.getSource()).getScene().getWindow();
         scene = FXMLLoader.load(getClass().getResource("sample.Login.fxml"));
         stage.setScene(new Scene(scene));
         stage.show();
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
     }
 }
