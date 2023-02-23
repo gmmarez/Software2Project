abe5428ba6c9fc7a4c82e213ee8c0f86e727f627
gmmarez <gabrielmarez123@gmail.com>
2023-02-22 18:21:17 -0700

    2/22/23 8:21pm

---
+++ src/Model/Customer.java
@@ -0,0 +1,68 @@
+package Model;
+
+public class Customer {
+
+    private int customerId;
+    private String customerName;
+    private String customerAddress;
+    private int customerPostalCode;
+    private int customerPhone;
+    private int divisionId;
+
+    public Customer(int customerId, String customerName, String customerAddress, int customerPostalCode, int customerPhone, int divisionId) {
+        this.customerId = customerId;
+        this.customerName = customerName;
+        this.customerAddress = customerAddress;
+        this.customerPostalCode = customerPostalCode;
+        this.customerPhone = customerPhone;
+        this.divisionId = divisionId;
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerAddress() {
+        return customerAddress;
+    }
+
+    public void setCustomerAddress(String customerAddress) {
+        this.customerAddress = customerAddress;
+    }
+
+    public int getCustomerPostalCode() {
+        return customerPostalCode;
+    }
+
+    public void setCustomerPostalCode(int customerPostalCode) {
+        this.customerPostalCode = customerPostalCode;
+    }
+
+    public int getCustomerPhone() {
+        return customerPhone;
+    }
+
+    public void setCustomerPhone(int customerPhone) {
+        this.customerPhone = customerPhone;
+    }
+
+    public int getDivisionId() {
+        return divisionId;
+    }
+
+    public void setDivisionId(int divisionId) {
+        this.divisionId = divisionId;
+    }
+}
