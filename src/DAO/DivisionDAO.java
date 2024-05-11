0892cefae405a7ad0d8a1c396903e76105694294
gmmarez <gabrielmarez123@gmail.com>
2024-05-10 21:14:52 -0600

    5/10/2024 23:14

--- src/DAO/DivisionDAO.java
+++ src/DAO/DivisionDAO.java
@@ -4,7 +4,6 @@ import Main.JDBC;
 import Model.Divisions;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
