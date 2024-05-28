07c63a62649666b70095ed67a0fb85b7cdba6ba1
gmmarez <gabrielmarez123@gmail.com>
2024-05-27 18:42:38 -0600

    5/27/2024 20:30

--- src/DAO/CustomerDAO.java
+++ src/DAO/CustomerDAO.java
@@ -8,6 +8,8 @@ import Model.Customers;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
 
 public class CustomerDAO {
     public static ObservableList<Customers> getAllCustomers() throws SQLException {
@@ -31,6 +33,21 @@ public class CustomerDAO {
         throw new RuntimeException(exception);
     }
         return customersObservableList;
+    }
+
+    public static void addCustomer(String customerName, String customerAddress, String customerPostalCode,
+                                   String customerPhone, LocalDateTime createdDate, LocalDateTime lastUpdated,
+                                   int divisionId) throws SQLException {
+        String sql = "INSERT INTO customers (Customer_Name, Address, Postal_Code, Phone, Create_Date, Last_Update, Division_ID) VALUES (?, ?, ?, ?, ?, ?, ?)";
+        PreparedStatement insertCustomer = JDBC.conn.prepareStatement(sql);
+        insertCustomer.setString(1, customerName);
+        insertCustomer.setString(2, customerAddress);
+        insertCustomer.setString(3, customerPostalCode);
+        insertCustomer.setString(4, customerPhone);
+        insertCustomer.setTimestamp(5, Timestamp.valueOf(createdDate));
+        insertCustomer.setTimestamp(6, Timestamp.valueOf(lastUpdated));
+        insertCustomer.setInt(7, divisionId);
+        insertCustomer.executeUpdate();
     }
 
 }
