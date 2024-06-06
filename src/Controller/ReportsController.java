8c4c6674cfbc694aced3aff97aaa778ec889cf99
gmmarez <gabrielmarez123@gmail.com>
2024-06-05 23:37:25 -0600

    6/6/2024 01:37

--- src/Controller/ReportsController.java
+++ src/Controller/ReportsController.java
@@ -1,18 +1,35 @@
 package Controller;
 
+import Model.Appointments;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.stage.Stage;
 import java.io.IOException;
+import java.net.URL;
+import java.sql.Timestamp;
+import java.util.ResourceBundle;
 
-public class ReportsController {
+public class ReportsController implements Initializable {
 
-    @FXML
-    public Button ReportsBack;
+    @FXML public Button ReportsBack;
+
+    @FXML private TableView ContactTable;
+    @FXML private TableColumn<Appointments, Integer> contactAppointmentId;
+    @FXML private TableColumn<Appointments, String> contactAppointmentTitle;
+    @FXML private TableColumn<Appointments, String> contactAppointmentDescription;
+    @FXML private TableColumn<Appointments, Integer> contactAppointmentContact;
+    @FXML private TableColumn<Appointments, Timestamp> contactAppointmentStart;
+    @FXML private TableColumn<Appointments, Timestamp> contactAppointmentEnd;
+    @FXML private TableColumn<Appointments, String> contactAppointmentType;
+    @FXML private TableColumn<Appointments, Integer> contactAppointmentCustomerId;
+    @FXML private TableColumn<Appointments, Integer> contactAppointmentUserId;
 
     Stage stage;
     Parent scene;
@@ -24,6 +41,10 @@ public class ReportsController {
         scene = FXMLLoader.load(getClass().getResource("../View/MainMenu.fxml"));
         stage.setScene(new Scene(scene));
         stage.show();
-
         }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
     }
+}
