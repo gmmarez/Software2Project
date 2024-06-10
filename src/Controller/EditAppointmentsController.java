cbe4ee280411853ee5a101a5a2edebe87e5d9e1f
gmmarez <gabrielmarez123@gmail.com>
2024-06-09 23:34:08 -0600

    6/10/2024 01:33

--- src/Controller/EditAppointmentsController.java
+++ src/Controller/EditAppointmentsController.java
@@ -3,6 +3,7 @@ package Controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -10,8 +11,10 @@ import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class EditAppointmentsController {
+public class EditAppointmentsController implements Initializable {
     @FXML
     private TextField EditAppointmentAppointmentId;
 
@@ -64,64 +67,6 @@ public class EditAppointmentsController {
     Parent scene;
 
     @FXML
-    void EditAppointmentAppointmentId(ActionEvent event) {
-    }
-
-    @FXML
-    void EditAppointmentTitle(ActionEvent event) {
-    }
-
-    @FXML
-    void EditAppointmentDescription(ActionEvent event) {
-    }
-
-    @FXML
-    void EditAppointmentLocation(ActionEvent event) {
-    }
-
-    @FXML
-    void EditAppointmentType(ActionEvent event) {
-    }
-
-    @FXML
-    void EditAppointmentStartTime(ActionEvent event) {
-    }
-
-    @FXML
-    void EditAppointmentEndTime(ActionEvent event) {
-    }
-
-
-
-    @FXML
-    public void EditAppointmentCreateDate(ActionEvent event) {
-    }
-
-    @FXML
-    public void EditAppointmentCreatedBy(ActionEvent event) {
-    }
-
-    @FXML
-    public void EditAppointmentLastUpdate(ActionEvent event) {
-    }
-
-    @FXML
-    public void EditAppointmentLastUpdatedBy(ActionEvent event) {
-    }
-
-    @FXML
-    public void EditAppointmentCustomerId(ActionEvent event) {
-    }
-
-    @FXML
-    public void EditAppointmentUserId(ActionEvent event) {
-    }
-
-    @FXML
-    public void EditAppointmentContactId(ActionEvent event) {
-    }
-
-    @FXML
     void AddAppointmentsSave(ActionEvent event) {
     }
 
@@ -132,5 +77,10 @@ public class EditAppointmentsController {
         scene = FXMLLoader.load(getClass().getResource("../View/Appointments.fxml"));
         stage.setScene(new Scene(scene));
         stage.show();
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        // Set selected appointment information
     }
 }
--- src/Controller/EditCustomersController.java
+++ src/Controller/EditCustomersController.java
@@ -3,6 +3,7 @@ package Controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
@@ -10,8 +11,10 @@ import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class EditCustomersController {
+public class EditCustomersController implements Initializable {
     @FXML
     private TextField EditCustomerCustomerId;
 
@@ -28,18 +31,6 @@ public class EditCustomersController {
     private TextField EditCustomerPhone;
 
     @FXML
-    public TextField EditCustomerLastUpdatedBy;
-
-    @FXML
-    public TextField EditCustomerLastUpdate;
-
-    @FXML
-    public TextField EditCustomerCreatedBy;
-
-    @FXML
-    public TextField EditCustomerCreateDate;
-
-    @FXML
     public ComboBox EditCustomerDivisionId;
     @FXML
     public ComboBox EditCustomerCountryId;
@@ -54,50 +45,6 @@ public class EditCustomersController {
     Parent scene;
 
     @FXML
-    void EditCustomerCustomerId(ActionEvent event) {
-    }
-
-    @FXML
-    void EditCustomerName(ActionEvent event) {
-    }
-
-    @FXML
-    void EditCustomerAddress(ActionEvent event) {
-    }
-
-    @FXML
-    void EditCustomerPostalCode(ActionEvent event) {
-    }
-
-    @FXML
-    void EditCustomerPhone(ActionEvent event) {
-    }
-
-    @FXML
-    public void EditCustomerLastUpdatedBy(ActionEvent event) {
-    }
-
-    @FXML
-    public void EditCustomerLastUpdate(ActionEvent event) {
-    }
-
-    @FXML
-    public void EditCustomerCreatedBy(ActionEvent event) {
-    }
-
-    @FXML
-    public void EditCustomerCreateDate(ActionEvent event) {
-    }
-
-    @FXML
-    public void EditCustomerDivisionId(ActionEvent event) {
-    }
-
-    @FXML
-    public void EditCustomerCountryId(ActionEvent event) {
-    }
-
-    @FXML
     void AddCustomersSave(ActionEvent event) {
     }
 
@@ -108,5 +55,10 @@ public class EditCustomersController {
         scene = FXMLLoader.load(getClass().getResource("../View/Customers.fxml"));
         stage.setScene(new Scene(scene));
         stage.show();
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        // Set selected customer information
     }
 }
